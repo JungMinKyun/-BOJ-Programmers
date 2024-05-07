070482de2b8d9a950da6a93e76551109778d2ad9
JungMinKyun <minkyun0418@gmail.com>
2024-05-07 21:58:24 +0900

    [ADD] BOJ source code

---
+++ 백준/11444.py
@@ -0,0 +1,28 @@
+# 백준 피보나치 수 6
+
+import sys
+from collections import defaultdict
+
+input = sys.stdin.readline
+
+mod = 1000000007
+dp = defaultdict(int)
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
+
+def fibo(n):
+    if not dp[n]:
+        if n & 1:
+            fn = fibo(n // 2)
+            fnp = fibo(n // 2 + 1)
+            dp[n] = (fn ** 2 + fnp ** 2) % mod
+        else:
+            fn = fibo(n // 2)
+            fnm = fibo(n // 2 - 1)
+            dp[n] = (fn * (fn + 2 * fnm)) % mod
+    return dp[n]
+
+n = int(input())
+fibo(n)
+print(dp[n])
