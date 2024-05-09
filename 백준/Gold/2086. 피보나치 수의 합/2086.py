d49163124ff472a0a1e2a765cf6d7f0f2ae856c0
JungMinKyun <minkyun0418@gmail.com>
2024-05-10 08:22:44 +0900

    [ADD] BOJ source code

---
+++ 백준/Gold/2086. 피보나치 수의 합/2086.py
@@ -0,0 +1,27 @@
+# 백준 2086 피보나치 수의 합
+
+import sys
+from collections import defaultdict
+
+input = sys.stdin.readline
+
+mod = 1000000000
+dp = defaultdict(int)
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
+
+def fibo(n):
+    if not dp[n]:
+        if n & 1:
+            fn = fibo(n // 2)
+            fnp = fibo(n // 2 + 1)
+            dp[n] = (fn ** 2 + fnp ** 2) % mod
+        else:
+            fn = fibo(n // 2)
+            fnm = fibo(n // 2 - 1)
+            dp[n] = (fn * (fn + 2 * fnm)) % mod
+    return dp[n]
+
+a, b = map(int, input().split())
+print((fibo(b + 2) - fibo(a + 1) + mod) % mod)
