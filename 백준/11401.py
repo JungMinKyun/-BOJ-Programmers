b9e7d045c99692baaa03393392579010513d7acb
JungMinKyun <minkyun0418@gmail.com>
2024-05-15 10:46:21 +0900

    [ADD] BOJ source code

---
+++ 백준/11401.py
@@ -0,0 +1,20 @@
+# 백준 11401 이항 계수 3
+
+mod = 1000000007
+
+def power_(a,b,c):
+    if b == 1:
+        return a % c
+    temp = power_(a, b // 2, c)
+    if b % 2 == 0 :
+        return temp * temp % c
+    else:
+        return temp * temp * a % c
+
+n, k = map(int, input().split())
+
+factorials = [1] * (n+1)
+for i in range(2, n+1):
+    factorials[i] = factorials[i-1] * i % mod
+
+print(factorials[n] * power_(factorials[k]*factorials[n-k], mod-2, mod) % mod)
