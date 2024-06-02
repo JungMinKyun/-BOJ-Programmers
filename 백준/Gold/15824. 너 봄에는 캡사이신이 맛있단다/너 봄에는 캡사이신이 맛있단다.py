c318b53b51d4a042da2107b4c715e6e4fa6fef6b
JungMinKyun <142962182+JungMinKyun@users.noreply.github.com>
2024-06-02 14:50:05 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 2508 ms, Memory: 66600 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -3,20 +3,22 @@
 import sys
 input = sys.stdin.readline
 
+mod = 1000000007
+
+def pow(a, b):
+    if b == 0:
+        return 1
+    if b == 1:
+        return a
+    half = pow(a, b//2)
+    return half * half % mod if b % 2 == 0 else half * half * a % mod
+
 n = int(input())
 menus = list(map(int, input().split()))
 menus.sort()
 
-mod = 1000000007
-twos = [1] * (n+1)
-for i in range(1, n+1):
-    twos[i] = twos[i-1] * 2 % mod
-
 result = 0 
 for i in range(n):
-    for j in range(n-1, i, -1):
-        value = menus[j] - menus[i]
-        value *= twos[j-i-1]
-        result += value % mod
+    result += menus[i] * (pow(2, i) - pow(2, n-i-1))
 
 print(result % mod)
