f4a0f3fc34ebfb5277bdbde00acf188f986ca1b6
JungMinKyun <minkyun0418@gmail.com>
2024-06-06 10:50:19 +0900

    [ADD] BOJ source code

--- 백준/Gold/1915. 가장 큰 정사각형/1915.py
+++ 백준/Gold/1915. 가장 큰 정사각형/1915.py
@@ -19,22 +19,3 @@ for i in range(n):
         ans = max(ans, dp[i][j])
 
 print(ans ** 2)
-
-
-# if n > 1 and m > 1:
-#     for i in range(1, n):
-#         for j in range(1, m):
-#             if dp[i][j] == 1:
-#                 ans = max(ans, 1)
-#             if dp[i-1][j-1] == dp[i-1][j] == dp[i][j-1] and dp[i-1][j-1]+1 == dp[i][j]:
-#                 ans = max(ans, dp[i][j])
-            
-# elif n == 1:
-#     ans = max(dp[0])
-# elif m == 1:
-#     for i in range(n):
-#         ans = max(dp[i][0], ans) 
-# else:
-#     ans = dp[0][0]
-
-# print(ans ** 2)
