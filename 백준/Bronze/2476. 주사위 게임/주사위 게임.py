a99ea3af9ce4b459ff213ff8641ae98781dca39a
JungMinKyun <142962182+JungMinKyun@users.noreply.github.com>
2024-10-25 08:14:17 +0900

    [Bronze III] Title: 주사위 게임, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -0,0 +1,17 @@
+# 백준 2476 주사위 게임
+
+n = int(input())
+ans = 0
+
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    if a == b == c:
+        ans = max(ans, 10000 + a * 1000)
+    elif a == b or a == c:
+        ans = max(ans, 1000 + a * 100)
+    elif b == c:
+        ans = max(ans, 1000 + b * 100)
+    else:
+        ans = max(ans, max(a,b,c) * 100)
+
+print(ans)
